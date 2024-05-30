957e4d8aedfff571a0a0a0b2ae41c76a9629cee6
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-05-29 20:24:14 -0500

    Initial Commit

---
+++ AsyncImageSwiftUI/AsyncImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSwiftUIApp.swift
+//  AsyncImageSwiftUI
+//
+//  Created by Oscar David Myerston Vega on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
